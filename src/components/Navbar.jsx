7c54f635b9bc603aa94c2accf6d0aba55734b984
unknown <calvinp003@gmail.com>
2020-03-10 10:44:54 -0700

    work on navbar border stuff that I am probably about to scrap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,32 +4,77 @@ import React from 'react';
 function Navbar() {
   return (
     <div className="headerWrap">
+      <div className="topLeftRectangle">
+        <div className="innerTopLeft">
+          <div className="rightSideBorder">
+          <div className="topLeftBlackRectangle">
+          </div>
+        </div>
+        </div>
+      </div>
       <div className="outerHeader">
         <div className="innerHeader">
           <p>Tier List</p>
-        <p>Characters</p>
-        <p>Info</p>
-      </div>
+          <p>Characters</p>
+          <p>Info</p>
+        </div>
       </div>
       <style jsx>{`
           .headerWrap{
+            background-color: #FF9A23;
+            border-top: 1px solid black;
+            border-bottom: 1px solid black;
+            width: 101%;
+            margin-left: -.5vw;
+          }
+          .topLeftRectangle{
+            width: 120px;
+            height: 15px;
+            transform: skew(-20deg);
+            background-color: #FF6F17;
+            margin-bottom: -2vh;
+            border-bottom: 2px solid black;
+            border-right: 2px solid black;
+            margin-left: -1vw;
+          }
+          .rightSideBorder{
+            width: 125px;
+            height: 4px;
+            background-color: #FF6F17;
+            margin-bottom: 6vh;
 
-            border: 1px solid black;
+          }
+          .topLeftBlackRectangle{
+            width: 115px;
+            height: 12px;
+            background-color: black;
+            transform: skew(-20deg);
+          }
+          .innerTopLeft{
+            border-bottom: 1px solid black;
+            // border-right: 1px solid black;
+            width: 115px;
+            height: 13px;
+            margin-bottom: -100px;
+            // background-color: black;
+
           }
           .outerHeader{
 
-            border: 5px solid orange;
+            border-top: 5px solid #FF6F17;
+            border-bottom: 5px solid #FF6F17;
           }
           .innerHeader{
             display:flex;
             flex-flow: row-wrap;
             justify-content: space-between;
-            border: 1px solid black
+            border-top: 1px solid black;
+            border-bottom: 1px solid black;
           }
 
           `}</style>
-        </div>
-      );
-    }
+      </div>
+    );
+  }
 
-    export default Navbar;
+  export default Navbar;
