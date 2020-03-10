aa96e740d4114ab4b27a15fa009f17314945a81e
unknown <calvinp003@gmail.com>
2020-03-10 12:50:59 -0700

    put a pause on the rectangle border for now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,6 +26,16 @@ function Navbar() {
       <div className="topRightinnerRectangle">
       </div>
     </div>
+      <div className="bottomLeftRectangle">
+        <div className="bottomLeftouterRectangleWrap">
+        </div>
+        <div className="bottomLeftouterRectangle">
+        </div>
+        <div className="bottomLeftinnerRectangleWrap">
+        </div>
+        <div className="bottomLeftinnerRectangle">
+        </div>
+      </div>
       <div className="innerHeader">
         <p>Tier List</p>
         <p>Characters</p>
@@ -40,77 +50,113 @@ function Navbar() {
             border-bottom: 1px solid black;
             width: 101%;
             margin-left: -.5vw;
-          }
-          .topRightRectangle{
-            float: right;
-            margin-top: -1.4vh;
-            margin-right: -1.3vw;
-          }
-          .topRightinnerRectangle{
-            width: 118px;
-            height: 20px;
-            transform: skew(30deg);
-            margin-top: -2.8vh;
-
-            background-color: black;
-            border-top: 1px solid black;
-          }
-          .topRightinnerRectangleWrap{
-            width: 90px;
-            height: 10px;
-            transform: skew(20deg);
-            margin-top: -1.7vh;
-            margin-left: .3vw;
-            border-bottom: 1px solid black;
-          }
-          .topRightouterRectangleWrap{
-            width: 100px;
-            height: 30px;
-            transform: skew(20deg);
-            background:#FF6F17;
-            margin-top: -.8vh;
-          }
-          .topRightouterRectangle{
-            width: 99px;
-            height: 25px;
-            transform: skew(27deg);
-            background:#FF6F17;
-            margin-top: -2.5vh;
-            border-left: 2px solid black;
-            border-bottom: 2px solid black;
-          }
-          .topLeftinnerRectangle{
-            width: 118px;
-            height: 20px;
-            transform: skew(-30deg);
-            margin-top: -2.8vh;
-            margin-left: -1vw;
-            background-color: black;
-            border-top: 1px solid black;
-          }
-          .topLeftinnerRectangleWrap{
-            width: 96px;
-            height: 10px;
-            transform: skew(-20deg);
-            margin-top: -1.7vh;
-            border-bottom: 1px solid black;
-          }
-          .topLeftouterRectangleWrap{
-            width: 100px;
-            height: 30px;
-            transform: skew(-20deg);
-            background:#FF6F17;
-            margin-top: -.8vh;
-          }
-          .topLeftouterRectangle{
-            width: 99px;
-            height: 25px;
-            transform: skew(-20deg);
-            background:#FF6F17;
-            margin-top: -2.5vh;
-            border-right: 2px solid black;
-            border-bottom: 2px solid black;
-          }
+          // }
+          // .topRightRectangle{
+          //   float: right;
+          //   margin-top: -1.4vh;
+          //   margin-right: -1.3vw;
+          // }
+          // .topRightinnerRectangle{
+          //   width: 118px;
+          //   height: 20px;
+          //   transform: skew(30deg);
+          //   margin-top: -2.8vh;
+          //
+          //   background-color: black;
+          //   border-top: 1px solid black;
+          // }
+          // .topRightinnerRectangleWrap{
+          //   width: 90px;
+          //   height: 10px;
+          //   transform: skew(20deg);
+          //   margin-top: -1.7vh;
+          //   margin-left: .3vw;
+          //   border-bottom: 1px solid black;
+          // }
+          // .topRightouterRectangleWrap{
+          //   width: 100px;
+          //   height: 30px;
+          //   transform: skew(20deg);
+          //   background:#FF6F17;
+          //   margin-top: -.8vh;
+          // }
+          // .topRightouterRectangle{
+          //   width: 99px;
+          //   height: 25px;
+          //   transform: skew(27deg);
+          //   background:#FF6F17;
+          //   margin-top: -2.5vh;
+          //   border-left: 2px solid black;
+          //   border-bottom: 2px solid black;
+          // }
+          // .topLeftinnerRectangle{
+          //   width: 118px;
+          //   height: 20px;
+          //   transform: skew(-30deg);
+          //   margin-top: -2.8vh;
+          //   margin-left: -1vw;
+          //   background-color: black;
+          //   border-top: 1px solid black;
+          // }
+          // .topLeftinnerRectangleWrap{
+          //   width: 96px;
+          //   height: 10px;
+          //   transform: skew(-20deg);
+          //   margin-top: -1.7vh;
+          //   border-bottom: 1px solid black;
+          // }
+          // .topLeftouterRectangleWrap{
+          //   width: 100px;
+          //   height: 30px;
+          //   transform: skew(-20deg);
+          //   background:#FF6F17;
+          //   margin-top: -.8vh;
+          // }
+          // .topLeftouterRectangle{
+          //   width: 99px;
+          //   height: 25px;
+          //   transform: skew(-20deg);
+          //   background:#FF6F17;
+          //   margin-top: -2.5vh;
+          //   border-right: 2px solid black;
+          //   border-bottom: 2px solid black;
+          // }
+          // .bottomLeftRectangle{
+          //   margin-top: 4vh;
+          // }
+          // .bottomLeftinnerRectangle{
+          //   width: 118px;
+          //   height: 20px;
+          //   transform: skew(30deg);
+          //
+          //   margin-left: -1vw;
+          //   background-color: black;
+          //   border-bottom: 1px solid black;
+          // }
+          // .bottomLeftinnerRectangleWrap{
+          //   width: 90px;
+          //   height: 10px;
+          //   transform: skew(20deg);
+          //   margin-top: -1.7vh;
+          //   margin-left: .3vw;
+          //   border-top: 1px solid black;
+          // }
+          // .bottomLeftouterRectangleWrap{
+          //   width: 100px;
+          //   height: 30px;
+          //   transform: skew(20deg);
+          //   background:#FF6F17;
+          //   margin-top: -.8vh;
+          // }
+          // .bottomLeftouterRectangle{
+          //   width: 99px;
+          //   height: 25px;
+          //   transform: skew(27deg);
+          //   background:#FF6F17;
+          //   margin-top: -2.5vh;
+          //   border-right: 2px solid black;
+          //   border-top: 2px solid black;
+          // }
           .topBar{
             width: 90vw;
             height: 5px;
