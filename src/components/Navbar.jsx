13b08b000e6424b3cdde48e71eb277fb45b1200e
unknown <calvinp003@gmail.com>
2020-03-09 19:26:34 -0700

    style navbar more

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,17 +4,32 @@ import React from 'react';
 function Navbar() {
   return (
     <div className="headerWrap">
-    <p>Tier List</p>
-    <p>Characters</p>
-    <p>Info</p>
-    <style jsx>{`
-        .headerWrap{
-          display:flex;
-          flex-flow: row-wrap;
-          justify-content: space-between;
-        }`}</style>
-    </div>
-  );
-}
+      <div className="outerHeader">
+        <div className="innerHeader">
+          <p>Tier List</p>
+        <p>Characters</p>
+        <p>Info</p>
+      </div>
+      </div>
+      <style jsx>{`
+          .headerWrap{
 
-export default Navbar;
+            border: 1px solid black;
+          }
+          .outerHeader{
+
+            border: 5px solid orange;
+          }
+          .innerHeader{
+            display:flex;
+            flex-flow: row-wrap;
+            justify-content: space-between;
+            border: 1px solid black
+          }
+
+          `}</style>
+        </div>
+      );
+    }
+
+    export default Navbar;
