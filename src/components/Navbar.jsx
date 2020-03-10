70a94cdc464abb2cf448c88e8b0ac9522cd81a04
unknown <calvinp003@gmail.com>
2020-03-10 12:36:27 -0700

    top right rectangle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,26 @@ function Navbar() {
     <div className="headerWrap">
       <div className="topBar">
       </div>
-      <div className="outerRectangleWrap">
+      <div class='topLeftRectangle'>
+      <div className="topLeftouterRectangleWrap">
       </div>
-      <div className="outerRectangle">
+      <div className="topLeftouterRectangle">
       </div>
-      <div className="innerRectangleWrap">
+      <div className="topLeftinnerRectangleWrap">
       </div>
-      <div className="innerRectangle">
+      <div className="topLeftinnerRectangle">
       </div>
+      </div>
+      <div className='topRightRectangle'>
+      <div className="topRightouterRectangleWrap">
+      </div>
+      <div className="topRightouterRectangle">
+      </div>
+      <div className="topRightinnerRectangleWrap">
+      </div>
+      <div className="topRightinnerRectangle">
+      </div>
+    </div>
       <div className="innerHeader">
         <p>Tier List</p>
         <p>Characters</p>
@@ -29,30 +41,68 @@ function Navbar() {
             width: 101%;
             margin-left: -.5vw;
           }
-          .innerRectangle{
-            width: 119px;
-            height: 15px;
+          .topRightRectangle{
+            float: right;
+            margin-top: -1.4vh;
+            margin-right: -1.3vw;
+          }
+          .topRightinnerRectangle{
+            width: 118px;
+            height: 20px;
+            transform: skew(30deg);
+            margin-top: -2.8vh;
+
+            background-color: black;
+            border-top: 1px solid black;
+          }
+          .topRightinnerRectangleWrap{
+            width: 90px;
+            height: 10px;
+            transform: skew(20deg);
+            margin-top: -1.7vh;
+            margin-left: .3vw;
+            border-bottom: 1px solid black;
+          }
+          .topRightouterRectangleWrap{
+            width: 100px;
+            height: 30px;
+            transform: skew(20deg);
+            background:#FF6F17;
+            margin-top: -.8vh;
+          }
+          .topRightouterRectangle{
+            width: 99px;
+            height: 25px;
+            transform: skew(27deg);
+            background:#FF6F17;
+            margin-top: -2.5vh;
+            border-left: 2px solid black;
+            border-bottom: 2px solid black;
+          }
+          .topLeftinnerRectangle{
+            width: 118px;
+            height: 20px;
             transform: skew(-30deg);
             margin-top: -2.8vh;
             margin-left: -1vw;
             background-color: black;
             border-top: 1px solid black;
           }
-          .innerRectangleWrap{
+          .topLeftinnerRectangleWrap{
             width: 96px;
             height: 10px;
             transform: skew(-20deg);
             margin-top: -1.7vh;
             border-bottom: 1px solid black;
           }
-          .outerRectangleWrap{
+          .topLeftouterRectangleWrap{
             width: 100px;
             height: 30px;
             transform: skew(-20deg);
             background:#FF6F17;
             margin-top: -.8vh;
           }
-          .outerRectangle{
+          .topLeftouterRectangle{
             width: 99px;
             height: 25px;
             transform: skew(-20deg);
